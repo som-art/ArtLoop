588dea87915d12782578cbc9577a8452bb0157be
Somosree Das <dassomosree59@gmail.com>
2024-10-21 17:23:03 +0530

    Added Notification controller and routes

---
+++ backend/controllers/notification.controller.js
@@ -0,0 +1,34 @@
+import Notification from "../models/notification.model.js";
+
+export const getNotifications = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    //Find the notifications send to that user by matching the userId to the 'to' field
+    const notifications = await Notification.find({ to: userId }).populate({
+      path: "from",
+      //Select just the username and profile img of the from field to display
+      select: "username profileImg",
+    });
+
+    //After opening the notification mark the new ones as read
+    await Notification.updateMany({ to: userId }, { read: true });
+
+    res.status(200).json(notifications);
+  } catch (error) {
+    console.log("Error in getNotifications function", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const deleteNotifications = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    await Notification.deleteMany({ to: userId });
+
+    res.status(200).json({ message: "Notifications deleted successfully" });
+  } catch (error) {
+    console.log("Error in deleteNotifications function", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
@@ -27,6 +28,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
